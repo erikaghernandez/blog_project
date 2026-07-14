7b6073a23a21215d2c45d20749cab7489e5fafdc
Erika Hernandez <erikaghernandez212@gmail.com>
2026-07-14 18:38:57 -0500

    config database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'blogusererika',
+        'PASSWORD': 'erika1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
